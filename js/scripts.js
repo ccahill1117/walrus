936a1a109da6b043d3ec12561682ac784f095cb1
christopher cahill and maly phongsavanh <christopher@>
2018-10-16 09:52:59 -0700

    adds orca page

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,25 @@
+// index.html jquery
+
 $(document).ready(function() {
   $(".clickable").click(function() {
     $("#clickshow").toggle();
     $("#clickhide").toggle();
   });
   $(".txtshow").click(function() {
-    $("#fade-in").slideUp();
+    $("#fade-in").fadeIn();
+    $("#regularetext").hide();
+  });
+});
+
+// orca.html jquey
+
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#clickshow").toggle();
+    $("#clickhide").toggle();
+  });
+  $(".txtshow").click(function() {
+    $("#fade-in").fadeIn();
     $("#regularetext").hide();
   });
 });
